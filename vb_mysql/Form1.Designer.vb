0f8858e4713bf0483efc963251d9e90cc7b84931
mengxyz <36928202+mengxyz@users.noreply.github.com>
2019-03-25 15:49:58 +0700

    first

---
+++ vb_mysql/Form1.Designer.vb
@@ -0,0 +1,108 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.btn_insert = New System.Windows.Forms.Button()
+        Me.btn_delete = New System.Windows.Forms.Button()
+        Me.btn_update = New System.Windows.Forms.Button()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 151)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(453, 480)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(71, 23)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(265, 20)
+        Me.TextBox1.TabIndex = 1
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(71, 49)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(265, 20)
+        Me.TextBox2.TabIndex = 2
+        '
+        'btn_insert
+        '
+        Me.btn_insert.Location = New System.Drawing.Point(45, 116)
+        Me.btn_insert.Name = "btn_insert"
+        Me.btn_insert.Size = New System.Drawing.Size(75, 23)
+        Me.btn_insert.TabIndex = 3
+        Me.btn_insert.Text = "insert"
+        Me.btn_insert.UseVisualStyleBackColor = True
+        '
+        'btn_delete
+        '
+        Me.btn_delete.Location = New System.Drawing.Point(144, 116)
+        Me.btn_delete.Name = "btn_delete"
+        Me.btn_delete.Size = New System.Drawing.Size(75, 23)
+        Me.btn_delete.TabIndex = 4
+        Me.btn_delete.Text = "Delete"
+        Me.btn_delete.UseVisualStyleBackColor = True
+        '
+        'btn_update
+        '
+        Me.btn_update.Location = New System.Drawing.Point(248, 116)
+        Me.btn_update.Name = "btn_update"
+        Me.btn_update.Size = New System.Drawing.Size(75, 23)
+        Me.btn_update.TabIndex = 5
+        Me.btn_update.Text = "Update"
+        Me.btn_update.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(477, 643)
+        Me.Controls.Add(Me.btn_update)
+        Me.Controls.Add(Me.btn_delete)
+        Me.Controls.Add(Me.btn_insert)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents btn_insert As System.Windows.Forms.Button
+    Friend WithEvents btn_delete As System.Windows.Forms.Button
+    Friend WithEvents btn_update As System.Windows.Forms.Button
+
+End Class
---
+++ vb_mysql/Form1.vb
@@ -0,0 +1,49 @@
+﻿Imports System.Data
+Imports MySql.Data.MySqlClient
+Public Class Form1
+    Dim conn As MySqlConnection = New MySqlConnection("Server=localhost;User Id=root; Password=; Database=mengx; Pooling=false")
+    Sub Page_Load()
+        conn.Open()
+        Dim sql As String = "select * from `user`"
+        Dim da As MySqlDataAdapter = New MySqlDataAdapter(sql, conn)
+        Dim ds As DataSet = New DataSet()
+        da.Fill(ds, "result")
+        DataGridView1.DataSource = ds
+        DataGridView1.DataMember = "result"
+        DataGridView1.ReadOnly = True
+        conn.Close()
+    End Sub
+    Sub clear()
+        TextBox1.Text = ""
+        TextBox2.Text = ""
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Page_Load()
+    End Sub
+
+    Private Sub btn_insert_Click(sender As Object, e As EventArgs) Handles btn_insert.Click
+        conn.Open()
+        Dim sql As String = "insert into `user` values('" & CStr(TextBox1.Text) & "','" & CStr(TextBox2.Text) & "')"
+        Dim sqlcmd As MySqlCommand = New MySqlCommand(sql, conn)
+        sqlcmd.ExecuteNonQuery()
+        conn.Close()
+        Page_Load()
+        clear()
+    End Sub
+
+    Private Sub btn_delete_Click(sender As Object, e As EventArgs) Handles btn_delete.Click
+        conn.Open()
+        Dim sql As String = "delete from `user` where id = '" & CStr(TextBox1.Text) & "'"
+        Dim sqlcmd As MySqlCommand = New MySqlCommand(sql, conn)
+        sqlcmd.ExecuteNonQuery()
+        conn.Close()
+        Page_Load()
+        clear()
+    End Sub
+
+    Private Sub DataGridView1_CellContentDoubleClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentDoubleClick
+        TextBox1.Text = DataGridView1.Rows(e.RowIndex).Cells(0).Value.ToString()
+        TextBox2.Text = DataGridView1.Rows(e.RowIndex).Cells(1).Value.ToString()
+    End Sub
+End Class
